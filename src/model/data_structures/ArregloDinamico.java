ec83a763bd288104765ce3b6b052dc408109c020
Porras53 <mf.porras@uniandes.edu.co>
2020-01-25 19:05:04 -0500

    Generic-Version

--- src/model/data_structures/ArregloDinamico.java
+++ src/model/data_structures/ArregloDinamico.java
@@ -7,7 +7,7 @@ package model.data_structures;
  * @author Fernando De la Rosa
  *
  */
-public class ArregloDinamico implements IArregloDinamico {
+public class ArregloDinamico <T extends Comparable<T>> implements IArregloDinamico<T> {
 		/**
 		 * Capacidad maxima del arreglo
 		 */
@@ -19,7 +19,7 @@ public class ArregloDinamico implements IArregloDinamico {
         /**
          * Arreglo de elementos de tamaNo maximo
          */
-        private String elementos[ ];
+        private Object elementos[ ];
 
         /**
          * Construir un arreglo con la capacidad maxima inicial.
@@ -27,18 +27,18 @@ public class ArregloDinamico implements IArregloDinamico {
          */
 		public ArregloDinamico( int max )
         {
-               elementos = new String[max];
+               elementos = new Object[max];
                tamanoMax = max;
                tamanoAct = 0;
         }
         
-		public void agregar( String dato )
+		public void agregar( T dato )
         {
                if ( tamanoAct == tamanoMax )
                {  // caso de arreglo lleno (aumentar tamaNo)
                     tamanoMax = 2 * tamanoMax;
-                    String [ ] copia = elementos;
-                    elementos = new String[tamanoMax];
+                    Object [ ] copia = elementos;
+                    elementos = new Object[tamanoMax];
                     for ( int i = 0; i < tamanoAct; i++)
                     {
                      	 elementos[i] = copia[i];
@@ -57,21 +57,52 @@ public class ArregloDinamico implements IArregloDinamico {
 			return tamanoAct;
 		}
 
-		public String darElemento(int i) {
+		public T darElemento(int i) {
 			// TODO implementar
-			return null;
+			return (T) elementos[i];
 		}
 
-		public String buscar(String dato) {
+		public T buscar(T dato) {
 			// TODO implementar
 			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
-			return null;
+			T resultado=null;
+			for(int i=0;i<tamanoAct;i++)
+			{
+				T actual= (T) elementos[i];
+				if(actual.compareTo(dato)==0)
+				{
+					resultado=actual;
+				}
+				
+			}
+			return resultado;
+		}
+		
+		public int darPosicion(T dato)
+		{
+			int resultado=0;
+			for(int i=0;i<tamanoAct;i++)
+			{
+				T actual= (T) elementos[i];
+				if(actual.compareTo(dato)==0)
+				{
+					resultado=i;
+				}
+				
+			}
+			return resultado;
 		}
 
-		public String eliminar(String dato) {
+		public T eliminar(T dato) {
 			// TODO implementar
 			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
-			return null;
+			int pos=darPosicion(dato);
+			Object[] resultado=new Object[elementos.length-1];
+			System.arraycopy(elementos, 0, resultado, 0, pos);
+			System.arraycopy(elementos, pos+1, resultado, pos, elementos.length-pos-1);
+			elementos= resultado;
+			
+			return dato;
 		}
 
 }
